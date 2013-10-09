70f5511e0e17037190d0445ea1b426108884d1dc
FrozenrainYoo <frozenrain.yoo@gmail.com>
2013-10-10 02:02:16 +0900

    [ror/server] first setup using rails-api

---
+++ quizbook/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_toke to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Quizbook::Application.config.secret_token = '23a1b66105dc001a36406637dbc51031fbfe63c25602d72e65e656e5593219ce9543528031082a275c848f805f805099034dea486969ab5cd5231814f91bc448'
